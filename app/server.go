2a4a5e9b6bd074b2288fca883d13d52f6856957a
ghkdxofla <ghkdxofla@gmail.com>
2024-12-02 23:58:55 +0900

    feat: add goroutine

--- app/server.go
+++ app/server.go
@@ -23,49 +23,57 @@ func main() {
 			os.Exit(1)
 		}
 
-		buf := make([]byte, 2048)
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-			os.Exit(1)
-		}
+		go func(conn net.Conn) {
+			defer func(conn net.Conn) {
+				err := conn.Close()
+				if err != nil {
+					fmt.Println("Error closing connection: ", err.Error())
+				}
+			}(conn)
 
-		requestLine, header, body := ParseRequest(string(buf[:n]))
+			buf := make([]byte, 2048)
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading:", err.Error())
+				return
+			}
 
-		fmt.Println("Received line: ", requestLine)
-		fmt.Println("Received header: ", header)
-		fmt.Println("Received body: ", body)
+			requestLine, header, body := ParseRequest(string(buf[:n]))
 
-		router := NewRouter()
-		AddEndpoint(router)
-		route := router.Find(requestLine)
-		response := &Response{}
+			fmt.Println("Received line: ", requestLine)
+			fmt.Println("Received header: ", header)
+			fmt.Println("Received body: ", body)
 
-		// TODO: router 처리 개선
-		if route == nil {
-			response = HandleRequest(Request{
-				RequestLine: requestLine,
-				Header:      header,
-				RequestBody: body,
-				PathParams:  nil,
-				QueryParams: nil,
-			}, nil)
-		} else {
-			response = HandleRequest(Request{
-				RequestLine: requestLine,
-				Header:      header,
-				RequestBody: body,
-				PathParams:  route.PathParams,
-				QueryParams: route.QueryParams,
-			}, route.Callback)
-		}
+			router := NewRouter()
+			AddEndpoint(router)
+			route := router.Find(requestLine)
+			response := &Response{}
 
-		fmt.Println("Response: ", response.ToString())
+			// TODO: router 처리 개선
+			if route == nil {
+				response = HandleRequest(Request{
+					RequestLine: requestLine,
+					Header:      header,
+					RequestBody: body,
+					PathParams:  nil,
+					QueryParams: nil,
+				}, nil)
+			} else {
+				response = HandleRequest(Request{
+					RequestLine: requestLine,
+					Header:      header,
+					RequestBody: body,
+					PathParams:  route.PathParams,
+					QueryParams: route.QueryParams,
+				}, route.Callback)
+			}
 
-		n, err = conn.Write([]byte(response.ToString()))
-		if err != nil {
-			fmt.Println("Error writing:", err.Error())
-			os.Exit(1)
-		}
+			fmt.Println("Response: ", response.ToString())
+
+			_, err = conn.Write([]byte(response.ToString()))
+			if err != nil {
+				fmt.Println("Error writing:", err.Error())
+			}
+		}(conn)
 	}
 }
